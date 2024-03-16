4630835d21b820926df1204878fc94fccc86f10f
adeelrauf <adeelrauf@gmail.com>
2024-03-17 02:00:23 +0500

    routes set up

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
 Route::post('login', 'UserController@login');
 Route::post('register', 'UserController@register');
+
+//for testing
 Route::get('/hello', function () {
     return 'Hello, World!';
 });
