aff433a9f01479b4c641f43ded330788ed04d37a
adeelrauf <adeelrauf@gmail.com>
2024-03-17 04:31:05 +0500

    api installed and tested using postman

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+
+//for testing
+Route::get('/hello', function () {
+    return 'Hello, World!';
+});
+ 
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::put('users/{id}', [UserController::class, 'update']);
+Route::delete('users/{id}', [UserController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,3 @@ use App\Http\Controllers\UserController;
 /* Route::get('/', function () {
     return view('welcome');
 }); */
-
-Route::post('login', 'UserController@login');
-Route::post('register', 'UserController@register');
-
-//for testing
-Route::get('/hello', function () {
-    return 'Hello, World!';
-});
-
-Route::post('login', 'UserController@login');
-Route::post('register', 'UserController@register');
-Route::get('/hello', function () {
-    return 'Hello, World!';
-});
-
-Route::get('users', [UserController::class, 'index']);
-Route::post('users', [UserController::class, 'store']);
-Route::get('users/{id}', [UserController::class, 'show']);
-Route::put('users/{id}', [UserController::class, 'update']);
-Route::delete('users/{id}', [UserController::class, 'destroy']);
